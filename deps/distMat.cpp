7d24f11d50d799b647df26100a1457fd16df394f
Anurag Yadav <yanurag994@gmail.com>
2024-07-21 03:15:18 -0400

    Minor Updates

--- deps/distMat.cpp
+++ deps/distMat.cpp
@@ -1,30 +1,33 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <numeric>
+#include <execution>
 
-double vectors_distance(const std::vector<double> &a, const std::vector<double> &b)
-{
-  std::vector<double> temp;
-  if (b.size() == 0)
-    return 0;
-  std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(temp), [](double e1, double e2)
-                 { return pow((e1 - e2), 2); });
-  return sqrt(std::accumulate(temp.begin(), temp.end(), 0.0));
+#ifdef _GLIBCXX_DEBUG 
+#include <stdexcept>
+#endif
+
+inline double vectors_distance(const std::vector<double>& a, const std::vector<double>& b) {
+#ifdef _GLIBCXX_DEBUG 
+  if (a.size() != b.size()) {
+    throw std::invalid_argument("Vectors must be of the same length");
+  }
+  if (a.empty()) {
+    throw std::invalid_argument("Vectors must not be empty");
+  }
+#endif
+  return sqrt(std::transform_reduce(std::execution::par, a.cbegin(), a.cend(), b.cbegin(), 0.0, std::plus<>(),
+    [](double e1, double e2) { return (e1 - e2) * (e1 - e2); }));
 }
 
-std::vector<std::vector<double>> distMat(std::vector<std::vector<double>> &inputData)
+std::vector<std::vector<double>> distMat(std::vector<std::vector<double>>& inputData)
 {
   std::vector<std::vector<double>> distMatrix;
-  if (distMatrix.size() > 0)
+  if (!distMatrix.empty())
     distMatrix.clear();
   distMatrix.resize(inputData.size(), std::vector<double>(inputData.size(), 0));
   for (size_t i = 0; i < inputData.size(); i++)
-  {
     for (size_t j = i + 1; j < inputData.size(); j++)
-    {
       distMatrix[i][j] = vectors_distance(inputData[i], inputData[j]);
-    }
-  }
   return distMatrix;
 }
--- deps/hopcroft_karp.cpp
+++ deps/hopcroft_karp.cpp
@@ -1,11 +1,23 @@
-
-
 #include "hopcroft_karp.hpp"
+
+HKGraph::HKGraph() = default;
+
+HKGraph::HKGraph(int m, int n)
+{
+    this->m = m;
+    this->n = n;
+    adj = std::vector<std::vector<int>>(m + 1);
+}
+
+void HKGraph::addEdge(int u, int v)
+{
+    adj[u].push_back(v);
+}
 
 std::vector<std::pair<int, int>> HKGraph::hopcroftKarpAlgorithm()
 {
-    pair_u = std::vector<int>(m + 1, NIL);
-    pair_v = std::vector<int>(n + 1, NIL);
+    pair_u = std::vector<int>(m + 1, 0);
+    pair_v = std::vector<int>(n + 1, 0);
 
     dist = std::vector<int>(m + 1);
     std::vector<std::pair<int, int>> matchingEdges;
@@ -13,7 +25,7 @@ std::vector<std::pair<int, int>> HKGraph::hopcroftKarpAlgorithm()
     {
         for (int u = 1; u <= m; u++)
         {
-            if (pair_u[u] == NIL && dfs(u))
+            if (pair_u[u] == 0 && dfs(u))
                 matchingEdges.emplace_back(u, pair_u[u]);
         }
     }
@@ -22,8 +34,8 @@ std::vector<std::pair<int, int>> HKGraph::hopcroftKarpAlgorithm()
 
 std::pair<std::set<int>, std::set<int>> HKGraph::custom_hopcroftKarpAlgorithm()
 {
-    pair_u = std::vector<int>(m + 1, NIL);
-    pair_v = std::vector<int>(n + 1, NIL);
+    pair_u = std::vector<int>(m + 1, 0);
+    pair_v = std::vector<int>(n + 1, 0);
 
     dist = std::vector<int>(m + 1);
     std::pair<std::set<int>, std::set<int>> matchingEdges;
@@ -31,7 +43,7 @@ std::pair<std::set<int>, std::set<int>> HKGraph::custom_hopcroftKarpAlgorithm()
     {
         for (int u = 1; u <= m; u++)
         {
-            if (pair_u[u] == NIL && dfs(u))
+            if (pair_u[u] == 0 && dfs(u))
             {
                 matchingEdges.first.insert(u);
                 matchingEdges.second.insert(pair_u[u]);
@@ -46,26 +58,26 @@ bool HKGraph::bfs()
     std::queue<int> q;
     for (int u = 1; u <= m; u++)
     {
-        if (pair_u[u] == NIL)
+        if (pair_u[u] == 0)
         {
             dist[u] = 0;
             q.push(u);
         }
         else
         {
-            dist[u] = INF;
+            dist[u] = INT_MAX;
         }
     }
-    dist[NIL] = INF;
+    dist[0] = INT_MAX;
     while (!q.empty())
     {
         int u = q.front();
         q.pop();
-        if (dist[u] < dist[NIL])
+        if (dist[u] < dist[0])
         {
             for (int v : adj[u])
             {
-                if (dist[pair_v[v]] == INF)
+                if (dist[pair_v[v]] == INT_MAX)
                 {
                     dist[pair_v[v]] = dist[u] + 1;
                     q.push(pair_v[v]);
@@ -73,41 +85,24 @@ bool HKGraph::bfs()
             }
         }
     }
-    return (dist[NIL] != INF);
+    return (dist[0] != INT_MAX);
 }
 
 bool HKGraph::dfs(int u)
 {
-    if (u != NIL)
+    if (u != 0)
     {
         for (auto v : adj[u])
         {
-            if (dist[pair_v[v]] == dist[u] + 1)
+            if (dist[pair_v[v]] == dist[u] + 1 && dfs(pair_v[v]))
             {
-                if (dfs(pair_v[v]) == true)
-                {
-                    pair_v[v] = u;
-                    pair_u[u] = v;
-                    return true;
-                }
+                pair_v[v] = u;
+                pair_u[u] = v;
+                return true;
             }
         }
-        dist[u] = INF;
+        dist[u] = INT_MAX;
         return false;
     }
     return true;
-}
-
-HKGraph::HKGraph() = default;
-
-HKGraph::HKGraph(int m, int n)
-{
-    this->m = m;
-    this->n = n;
-    adj = std::vector<std::vector<int>>(m + 1);
-}
-
-void HKGraph::addEdge(int u, int v)
-{
-    adj[u].push_back(v);
 }
