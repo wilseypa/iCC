d13b3074e318d0bcc4f50b10c5f82312a1b2bfad
Anurag Yadav <yanurag994@gmail.com>
2024-08-30 14:53:00 -0400

    Updates

--- deps/cycle_rm.hpp
+++ deps/cycle_rm.hpp
@@ -7,36 +7,37 @@
 
 #include "bi_graph.hpp"
 
-//d simplex traversal
-class Bi_Graph_Traversal {
+// d simplex traversal
+class Bi_Graph_Traversal
+{
 public:
-    //associated bi-graph
-    Bi_Graph* graphptr;
-    
-    std::vector<int> visit_flag;
+    // associated bi-graph
+    Bi_Graph *graphptr;
+
+    std::vector<int> root_flag;
 
     Bi_Graph_Traversal() = default;
-    Bi_Graph_Traversal(Bi_Graph*);
+    Bi_Graph_Traversal(Bi_Graph *);
 
     int findRootIterative(int);
-    
+
     std::vector<int> getParent(int uidx);
-    
+
     std::vector<int> getChild(int uidx);
 
     std::vector<int> getAncestor(int uidx);
 
-    bool isBackwardAcyclic(std::vector<int>&, std::vector<int>&, int);
+    bool isBackwardAcyclic(std::vector<int> &, std::vector<int> &, int);
 
-    int lookAheadDFS(std::queue<int>&, std::vector<int>&, int);
+    int lookAheadDFS(std::queue<int> &, std::vector<int> &, int);
 
-    int serialBFS(std::queue<int>&, int);
+    int serialBFS(std::queue<int> &, int);
 
     int serialCycleRemoval(int maxdegree);
 
-    void threadBackwardDFS(std::atomic<int>&, int&, int);
+    void threadBackwardDFS(int &, int);
 
-    int parallelBFS(std::queue<int>&, int, int, int);
+    int parallelBFS(int, int);
 
     int parallelRathod(int maxdegree, int threadnum);
 };
